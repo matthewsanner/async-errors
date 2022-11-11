5831023f877bdce92299e4548efe17b7a6dd5fe1
Matthew Sanner <gravityskunk@gmail.com>
2022-11-11 16:01:08 -0600

    initial commit

---
+++ models/product.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        //Replaces default message
+        required: [true, 'Name cannot be blank']
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    category: {
+        type: String,
+        lowercase: true,
+        enum: ['fruit', 'vegetable', 'dairy']
+    }
+})
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
